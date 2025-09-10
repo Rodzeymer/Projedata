8bf5b368c242e9d4608912089b073491867e03d3
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2025-09-09 22:11:56 -0300

    Importando bibliotecas

--- Principal.java
+++ Principal.java
@@ -1,1 +1,8 @@
-
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.*;
+import java.util.stream.Collectors;
